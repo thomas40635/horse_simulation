221166a00c19f2eac83b49be224e5767b70d56ac
Azaguilla <34061000+Azaguilla@users.noreply.github.com>
2019-02-22 15:08:55 +0100

    Ajout page Tableau de brd (accueil) (grossière) Ajout des actions sur le serveur mysql (start, stop, reload) -> à tester sur la VM de Sandrine

--- src/App/AdminBundle/Controller/DefaultController.php
+++ src/App/AdminBundle/Controller/DefaultController.php
@@ -3,11 +3,71 @@
 namespace App\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * Matches /
+     *
+     * @Route("/", name="index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('@AppAdmin/Default/index.html.twig');
+    }
+
+    /**
+     * Matches /start
+     *
+     * @Route("/start", name="start")
+     */
+    public function StartServerAction()
+    {
+        $messages = shell_exec("/etc/rc.d/init.d/mysql start");
+        return $this->redirectToRoute('server', array(
+            'messages' => $messages
+        ));
+    }
+
+    /**
+     * Matches /stop
+     *
+     * @Route("/stop", name="stop")
+     */
+    public function StopServerAction()
+    {
+        $messages = shell_exec("/etc/rc.d/init.d/mysql stop");
+        return $this->redirectToRoute('server', array(
+            'messages' => $messages
+        ));
+    }
+
+    /**
+     * Matches /reload
+     *
+     * @Route("/reload", name="reload")
+     */
+    public function ReloadServerAction()
+    {
+        $messages = shell_exec("/etc/rc.d/init.d/mysql reload");
+        return $this->redirectToRoute('server', array(
+            'messages' => $messages
+        ));
+    }
+
+    /**
+     * Matches /server
+     *
+     * @Route("/server", name="server")
+     */
+    public function ServerAction(Request $request)
+    {
+        $messages = $request->get('messages');
+        return $this->render('@AppAdmin/Default/server.html.twig', array(
+            'messages' => $messages
+        ));
     }
 }
