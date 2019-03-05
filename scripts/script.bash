e629337f136ff093a93d7f3d2746b7fd88ecf6df
Azaguilla <34061000+Azaguilla@users.noreply.github.com>
2019-03-05 13:12:54 +0100

    Ajout d'un script bash pour la tâche automatisée de surveillance

--- scripts/script.bash
+++ scripts/script.bash
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-# Vérifier les tables
-# -d donne les informations sur la table
-# -c vérifie si la table n'est pas corrompue
-verifTbales=$(myisamchk -d -c /var/lib/mysql/horse_simulation/*.MYI)
-echo $verifTbales
+tables=${cat /var/lib/mysql/horse_simulation/ | grep *.MYI}
+if  [ ! -z "$tables" ]
+    then
+		# Vérifier les tables
+		# -d donne les informations sur la table
+		# -c vérifie si la table n'est pas corrompue
+        verifTbales=$(myisamchk -d -c /var/lib/mysql/horse_simulation/*.MYI)
+        echo $verifTbales
 
-# Optimiser les tables
-# -a optimise les tables
-# -s trie l'arbre des index
-optimiseTables=$(myisamchk -a -s /var/lib/mysql/horse_simulation/*.MYI)
-echo $optimiseTables
+		# Optimiser les tables
+		# -a optimise les tables
+		# -s trie l'arbre des index
+		optimiseTables=$(myisamchk -a -s /var/lib/mysql/horse_simulation/*.MYI)
+		echo $optimiseTables
 
-# Défragmenter les tables
-# -r répare et défragmente les tables
-# -e vérifie de manière plus approfondie
-DefragTables=$(myisamchk -e -r /var/lib/mysql/horse_simulation/*.MYI)
-echo $DefragTables
+		# Défragmenter les tables
+		# -r répare et défragmente les tables
+		# -e vérifie de manière plus approfondie
+		DefragTables=$(myisamchk -e -r /var/lib/mysql/horse_simulation/*.MYI)
+		echo $DefragTables
+    else
+		echo "Il n'y a pas de fichier myisam dans la base."
+fi
+
+
+
+
